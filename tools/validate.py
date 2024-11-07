941ccd86846d7aa5d38e6d3488d3963b2c63eada
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2024-11-07 10:48:12 +0100

    Update validate.py

--- tools/validate.py
+++ tools/validate.py
@@ -143,27 +143,34 @@ for p in glob.iglob(dir_path_exemple+'/**/*.*', recursive=True):
                     except Exception as e: 
                         print("Erreur à  la transformation   : " + p)        
                         print(e)
-                        outputErreur += "<tr><td>" + p  + "</td><td>Erreur à la transformation  du rapport </td></tr>"       
+                        outputErreur += "<tr><td>" + p  + "</td><td>Erreur à la transformation  du rapport </td></tr>"   
+                        print("	 "<tr><td>" + p  + "</td><td> Erreur à la transformation  du rapport </td> <td></td> <td></td> <td></td> <td></td> <td></td>  </tr>" ,file=open(file_output, "a"))    
+                        
                 except  Exception as e: 
                     print("Erreur à  la recuperation du rapport de  validation  : " + p)    
                     print(e)
-                    outputErreur += "<tr><td>" + p  + "</td><td> Erreur à la récuperartion du rapport </td></tr>"         
+                    outputErreur += "<tr><td>" + p  + "</td><td> Erreur à la récuperartion du rapport </td></tr>"     
+                    print("	 "<tr><td>" + p  + "</td><td> Erreur à la récuperation du rapport </td> <td></td> <td></td> <td></td> <td></td> <td></td>  </tr>" ,file=open(file_output, "a"))    
+                    
             except Exception as e: 
                 print("Erreur à la validation  : " + p)
                 print(e)
                 outputErreur += "<tr><td>" + p  + "</td><td> Erreur à la validation </td></tr>"
+                print("	 "<tr><td>" + p  + "</td><td> Erreur à la validation </td> <td></td> <td></td> <td></td> <td></td> <td></td>  </tr>" ,file=open(file_output, "a"))    
 
 
         else :
             print("Fichier sans validateur  : " + p)  
             outputSansvalidateur += "<tr><td>" + p  + "</td><td>Pas de validateur trouvé </td></tr>"
+            print("	 "<tr><td>" + p  + "</td><td> Pas de validateur trouvé  </td> <td></td> <td></td> <td></td> <td></td> <td></td>  </tr>" ,file=open(file_output, "a"))    
         
-print("</table>" ,file=open(file_output, "a"))    
+
             
- 
+''' 
 if(outputErreur != "") :
     print("	<h2>:heavy_exclamation_mark: Fichier en erreur</h2><table>"  + outputErreur  + "</table>" ,file=open(file_output, "a"))    
 
 if(outputSansvalidateur != "") :
     print("	<h2>:heavy_exclamation_mark: Fichier sans validateur</h2><table>"  + outputSansvalidateur  + "</table>" ,file=open(file_output, "a"))    
-
+'''
+print("</table>" ,file=open(file_output, "a"))    
